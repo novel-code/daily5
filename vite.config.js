9b2eee145a4dd466d79aa80c91231de8819a184c
Muzzammil <76740147+novel-code@users.noreply.github.com>
2025-02-20 02:28:07 +0530

    Update vite.config.js to work with base route

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/daily5",
+  base: process.env.VITE_BASE_PATH || '/deploy_react_app_github_pages_vercel',
   plugins: [react()],
 });
